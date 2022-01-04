cf54c676e755ffae17060b2b3ece326d22981ca3
Dillon Smith <dsmith39@gmail.com>
2022-01-03 22:08:45 -0600

    added dist to .gitignore

--- dist/component.d.ts
+++ dist/component.d.ts
@@ -1,6 +1,13 @@
 interface ButtonProps {
+    /** The Button Text */
     label: string;
 }
+/**
+ * @function Button
+ * @param {string} label the text for the button
+ * @example
+ * <Button label="Button Text" />
+ */
 export const Button: (props: ButtonProps) => JSX.Element;
 
 //# sourceMappingURL=component.d.ts.map
--- dist/component.js
+++ dist/component.js
@@ -26,7 +26,12 @@ var $7be4ff376573ca5d$exports = {};
 
 $parcel$export($7be4ff376573ca5d$exports, "Button", () => $72051543a8425821$export$2e2bcd8739ae039);
 
-var $72051543a8425821$var$Button = function(props) {
+/**
+ * @function Button
+ * @param {string} label the text for the button
+ * @example
+ * <Button label="Button Text" />
+ */ var $72051543a8425821$var$Button = function(props) {
     return(/*#__PURE__*/ ($parcel$interopDefault($fUHUP$react)).createElement("button", null, props.label));
 };
 var $72051543a8425821$export$2e2bcd8739ae039 = $72051543a8425821$var$Button;
